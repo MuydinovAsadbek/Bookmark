9a46d3f64e0f82dbeeca7c33f76000259679933d
MuydinovAsadbek <asadbekmoydinov06@gmail.com>
2022-06-17 14:20:57 +0500

    Add js-tabs-item

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
+const elsTabItem = document.querySelectorAll('.feature__item');
+
+elsTabItem.forEach (function (elTabItem) {
+  elTabItem.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    elsTabItem.forEach(function(item) {
+      item.classList.remove('tabs-item-active');
+    });
+    elTabItem.classList.add('tabs-item-active')
+  });
+});
+
+
+// ACARDION
 const acardions = document.querySelectorAll(".acardion__item");
 
 acardions.forEach((acardion__item) => {
